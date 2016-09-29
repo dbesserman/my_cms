6c53e43833070f6dd6b69a210e427d07674f0c91
David Besserman <david.besserman@gmail.com>
2016-09-29 11:52:39 +0200

    main retrieves documents

--- cms.rb
+++ cms.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+DOCS_PATH = 'public/documents'
+
 get '/' do
-  'Getting started'
+  @documents = get_documents
+  require 'pry'; binding.pry
+end
+
+def get_documents
+  docs = Dir.entries(DOCS_PATH)
+  docs.shift(2) # removes '.' and '..'
+  docs
 end
