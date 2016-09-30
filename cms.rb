beda59cbd5ea7f9e4e910fab40d866803cce83d9
David Besserman <david.besserman@gmail.com>
2016-09-30 12:15:56 +0200

    dealing with non-existent files & reading Markdown files

--- cms.rb
+++ cms.rb
@@ -1,25 +1,56 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'redcarpet'
 
 DOCS_PATH = 'data'
 
+configure do
+  enable :sessions
+end
+
+before do
+  @documents = get_documents
+end
+
 # Displays links to available documents
 get '/' do
-  @documents = get_documents
   erb :index, layout: :layout
 end
 
 # Accesses a document
 get '/:document' do
   doc_name = params[:document]
-  headers['Content-Type'] = 'text/plain'
 
-  File.read("#{DOCS_PATH}/#{doc_name}")
+  if @documents.include?(doc_name)
+    load_file_content(doc_name)
+  else
+    session[:error] = "#{doc_name} does not exist."
+    redirect '/'
+  end
 end
 
+def load_file_content(file_name)
+  path = "#{DOCS_PATH}/#{file_name}"
+  content = File.read(path)
+
+  case File.extname(file_name)
+  when '.txt'
+    headers['Content-Type'] = 'text/plain'
+    content
+  when '.md'
+    render_markdown(content)
+  end
+end
+
+# Returns the name of the files in data
 def get_documents
   docs = Dir.entries(DOCS_PATH)
   docs.shift(2) # removes '.' and '..'
   docs
+end
+
+def render_markdown(content)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(content)
 end
--- test/test_cms.rb
+++ test/test_cms.rb
@@ -15,10 +15,9 @@ class CMSTest < Minitest::Test
   def test_index
     get '/'
     
-
     assert_equal 200, last_response.status
     assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
-    assert_includes last_response.body, 'about.txt'
+    assert_includes last_response.body, 'about.md'
     assert_includes last_response.body, 'history.txt'
     assert_includes last_response.body, 'changes.txt'
   end
@@ -29,5 +28,28 @@ class CMSTest < Minitest::Test
     assert_equal 200, last_response.status
     assert_equal 'text/plain', last_response['Content-Type']
     assert_includes last_response.body, 'Ruby 0.95 released'
+  end
+
+  def test_viewing_non_existing_document
+    document = 'some_document.txt'
+    error_message = "#{document} does not exist."
+
+    get "/#{document}"
+    assert_equal 302, last_response.status
+
+    get last_response['location']
+    assert_includes last_response.body, error_message
+
+    get '/'
+    refute_includes last_response.body, error_message
+  end
+
+  def test_viewing_markdown_file
+    converted_title = '<h1>Ruby is...</h1>'
+
+    get '/about.md' 
+    assert_equal 200, last_response.status
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+    assert_includes last_response.body, converted_title
   end
 end
